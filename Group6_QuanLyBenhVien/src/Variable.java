f9a434792d24c1149197164ed29995495b80f2e8
vuhongdang18092004 <hongdang18092004@gmail.com>
2025-05-07 03:08:34 +0700

    Complete Homework0

---
+++ Group6_QuanLyBenhVien/src/Variable.java
@@ -0,0 +1,24 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name = "John";
+        System.out.println(name);
+
+        int myNum = 15;
+        System.out.println(myNum);
+
+        int myNum1;
+        myNum1 = 15;
+        System.out.println(myNum1);
+
+        int myNum2 = 15;
+        myNum2 = 20;
+        System.out.println(myNum2);
+
+        int myNum3 = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+        System.out.println(myNum3 + " " + myFloatNum + " " + myLetter + " " + myBool + " " + myText);
+    }
+}
