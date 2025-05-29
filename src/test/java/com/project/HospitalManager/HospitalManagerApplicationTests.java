0353c3037fe150603209f1edbee5c2cc0093fbca
NguyenHieu-Class <22010103@st.phenikaa-uni.edu.vn>
2025-05-30 05:11:21 +0700

    spring boot

---
+++ src/test/java/com/project/HospitalManager/HospitalManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.HospitalManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
